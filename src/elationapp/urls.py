35f7b6e24f023436d68a4a895b245b336df9c10a
Abhinav <abhiasthana76@gmail.com>
2025-06-06 21:06:37 +0530

    fix: remove test url

--- src/elationapp/urls.py
+++ src/elationapp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.homepage, name=""),
-    path("test/", views.test_page, name="test"),
+    # path("test/", views.test_page, name="test"),
 ]
